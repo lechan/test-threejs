cf43a28fd689f7c5d3dfa3040b0b7288bee0e85c
lechan <lechan@126.com>
2023-10-18 16:32:55 +0800

    feat: 灯光学习

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import './App.scss'
-import HelloTexture from './components/hello-texture'
 // import HelloThreejs from '@/components/hello-threejs'
 // import HelloPrimitives from '@/components/hello-primitives'
 // import HelloScene from '@/components/hello-scene'
-HelloTexture
+// import HelloTexture from './components/hello-texture'
+import HelloLight from './components/hello-light';
 
 function App() {
   return (
     // <HelloThreejs />
     // <HelloPrimitives />
     // <HelloScene />
-    <HelloTexture />
+    // <HelloTexture />
+    <HelloLight />
   );
 }
 
